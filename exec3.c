5fa370ac936eaed32e5568c59b20213824a021f6
Igor Ramon <digorramon06@gmail.com>
2026-05-21 14:34:55 -0300

    Create exec3.c for temperature input and analysis
    Initialize a program to read and analyze temperatures.

---
+++ exec3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float temperaturas[7];
+    int c;
+
+    int seguras = 0;
+    int foraFaixa = 0;
+
+    float soma = 0;
+    float media;
+
+    for(c = 1; c < 8; c++){
+        printf("Digite a Temperatura %c: ", c);
+        scanf("%f", )
+    }
+    // 1. Ler as 7 temperaturas e armazenar no vetor temperaturas
+    // 2. Usar if com AND para verificar a faixa segura
+    // 3. Contar temperaturas seguras e fora da faixa
+    // 4. Calcular a média
+
+    return 0;
+}
